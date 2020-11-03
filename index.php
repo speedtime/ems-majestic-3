10747c7f38ec44fb548a545bc325364b632f415c
speedtime <dengrizel@gmail.com>
2020-11-04 03:41:38 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <title>EMS Soto4ka</title>
+<link rel="stylesheet" type="text/css" href="css\style.css">
+</head>
+<body>
+
+<div id="container">
+<!--	<?php include ("blocks/header.php");?> -->
+	<?php include ("blocks/navigation.php");?>
+	<?php include ("blocks/main.php");?>
+	<div id="content">
+	<h2>Хай</h2>
+	</div>
+		 
+	<?php include ("blocks/footer.php");?>
+
+
+</div>
+</body>
+
+</html>
